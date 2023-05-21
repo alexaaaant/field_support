37a6f01e251c9dcec92b7a4fcc672274fa620c05
alexaaaant <alexaaaant@gmail.com>
2023-05-21 16:05:19 +0300

    algorithm for finding devices for replacement

---
+++ src/common/helpers/batteriesParser.ts
@@ -0,0 +1,48 @@
+import { Academies, BatteriesRaw, Devices } from '../../types/batteries';
+
+export const batteriesParser = (data: BatteriesRaw) => {
+    const devices: Devices = {};
+    const academies: Academies = {};
+  
+    data.forEach(entry => {
+      if (!devices[entry.serialNumber]) {
+        devices[entry.serialNumber] = {
+          academyId: entry.academyId,
+          measurements: [],
+        };
+      }
+      devices[entry.serialNumber].measurements.push({
+        batteryLevel: entry.batteryLevel,
+        timestamp: new Date(entry.timestamp),
+      });
+    });
+  
+    Object.values(devices).forEach(device => {
+      device.measurements.sort((a, b) => +a.timestamp - +b.timestamp);
+      let totalBatteryUsage = 0;
+      let count = 0;
+  
+      for (let i = 1; i < device.measurements.length; i++) {
+        const previous = device.measurements[i - 1];
+        const current = device.measurements[i];
+        const timeDifference = (+current.timestamp - +previous.timestamp) / (1000 * 60 * 60 * 24);
+        const batteryUsage = previous.batteryLevel - current.batteryLevel;
+  
+        if (batteryUsage > 0) {
+          totalBatteryUsage += batteryUsage / timeDifference;
+          count++;
+        }
+      }
+  
+      if (count > 0 && totalBatteryUsage / count > 0.3) {
+        if (!academies[device.academyId]) {
+          academies[device.academyId] = { batteryIssues: 0, devices: [] };
+        }
+        academies[device.academyId].batteryIssues++;
+        academies[device.academyId].devices.push(device);
+      }
+    });
+  
+    return academies;
+  
+};
